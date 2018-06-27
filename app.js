9089b80b8eb8010e2e7f37cbc65850f4aa91ba25
urm <urm@il-urm01.(none)>
2018-06-27 10:26:19 +0300

    test js

---
+++ app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+
+const hostname = 'il-urm01';
+const port = 3000;
+
+//const server = http.createServer((req, res) => {
+//// above *wont work*?? below works
+var server = http.createServer(function(req, res){
+	res.statusCode = 200;
+   	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
+      });
+
+    // server.listen(port, hostname, () => {
+   // above *wont work*?? below works
+      server.listen(port, hostname, function() {
+        console.log('Server running at http://${hostname}:${port}/');
+      });
+
+//const http = require('http');
+//
+//const hostname = '127.0.0.1';
+//const port = 3000;
+//
+//const server = http.createServer((req, res) => {
+//  res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/plain');
+//  res.end('Hello World\n');
+//});
+//
+//server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//});
+
